7aa1e2dade73f97aca8ad9d54add5f169c6c6a1e
IamkatewithA <dhdructjvf@gmail.com>
2025-10-01 09:44:21 +0300

    добавление в main созданной функции

--- main.cpp
+++ main.cpp
@@ -63,6 +63,7 @@ int main() {
         std::cout << "Меню:\n";
         std::cout << "1. Добавить студента\n";
         std::cout << "2. Вывести список студентов\n";
+        std::cout << "3. Посчитать средний возраст студентов\n";
         std::cout << "0. Выход\n";
         std::cout << "Выберите действие: ";
         std::cin >> choice;
@@ -73,6 +74,9 @@ int main() {
                 break;
             case 2:
                 displayStudents(database);
+                break;
+            case 3:
+                calculateAverageAge(database);
                 break;
             case 0:
                 std::cout << "Выход из программы.\n";
